c2f69b32fc7f46ffe5a4d509b5f8f764e5109ecf
Scott Piriou <30843220+pscott@users.noreply.github.com>
2024-04-08 18:48:41 +0200

    stabilizor.sol

---
+++ src/Stabilizor.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+contract Stabilizor {
+    // stabilize(uint256[] calldata amounts) public 
+    function stabilizeMultiple(uint256[] calldata amounts) public {
+        // for each amount:
+        // transfer from alice
+        // transfer back to alice
+        //
+    }
+
+    function stabilize(uint256 amount) public {
+        // transfer from alice
+
+        // transfer back to alice
+    }
+}
